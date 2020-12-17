c59d388e2ab49c956db0ff02667f7260e6c0f06d
thanhhoa214 <hoa.nguyen@nng.bz>
2020-12-17 16:41:53 +0700

    First commit

---
+++ src/web.ts
@@ -0,0 +1,38 @@
+import { WebPlugin } from '@capacitor/core';
+import { ZaloAuthCapacitorPluginPlugin } from './definitions';
+
+export class ZaloAuthCapacitorPluginWeb
+  extends WebPlugin
+  implements ZaloAuthCapacitorPluginPlugin {
+  constructor() {
+    super({
+      name: 'ZaloAuthCapacitorPlugin',
+      platforms: ['web'],
+    });
+  }
+
+  getOAuthToken() {
+    return Promise.resolve({ token: 'Hello Web Plugin' });
+  }
+
+  async login() {
+    console.log('[login] Web version is not supported');
+    return Promise.resolve({
+      success: false,
+    });
+  }
+  async getUserProfile() {
+    console.log('[getUserProfile] Web version is not supported');
+    return { success: false, id: '', name: '' };
+  }
+  async logout() {
+    console.log('[logout] Web version is not supported');
+  }
+}
+
+const ZaloAuthCapacitorPlugin = new ZaloAuthCapacitorPluginWeb();
+
+export { ZaloAuthCapacitorPlugin };
+
+import { registerWebPlugin } from '@capacitor/core';
+registerWebPlugin(ZaloAuthCapacitorPlugin);
