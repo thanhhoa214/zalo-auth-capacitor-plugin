fc3a19ec6dec906526b6202ee54a77db57dd4e97
thanhhoa214 <hoa.nguyen@nng.bz>
2020-12-21 12:04:19 +0700

    Write function code

--- android/src/main/java/com/rin/zaloauth/ZaloAuthCapacitorPlugin.java
+++ android/src/main/java/com/rin/zaloauth/ZaloAuthCapacitorPlugin.java
@@ -1,17 +1,33 @@
 package com.rin.zaloauth;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+import com.zing.zalo.zalosdk.oauth.*;
+
 @NativePlugin
 public class ZaloAuthCapacitorPlugin extends Plugin {
 
     @PluginMethod
     public void login(PluginCall call) {
-        JSObject ret = new JSObject();
+        OAuthCompleteListener listener = new OAuthCompleteListener() {
+            @Override
+            public void onAuthenError(int errorCode, String message) {
+                //Đăng nhập thất bại..
+            }
+
+            @Override
+            public void onGetOAuthComplete(OauthResponse response) {
+                String code = response.getOauthCode();
+                //Đăng nhập thành công..
+            }
+        };
+        ZaloSDK.Instance.authenticate(this.getActivity(), LoginVia.APP_OR_WEB, listener);
         ret.put("token", "login from Android");
         call.success(ret);
     }
